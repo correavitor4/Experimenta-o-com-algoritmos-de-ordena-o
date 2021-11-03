90d0ec3716c95d5c8668de1b28f2bed25f9b5274
João Vitor Oliveira Corrêa <correavitor4@hotmail.com>
2021-11-02 21:03:18 -0300

    Conclusão do exercício

--- Counting Sort.c
+++ Counting Sort.c
@@ -8,10 +8,11 @@ void countingSort(int *a, int k,int tamanhoDoVetor, int *QtdOperacoesDeComparaca
 void printarVetor(int vetor[]);
 int main()
 {
-    prepararCountingSort(99999,1000);
-    prepararCountingSort(99999,10000);
-    prepararCountingSort(99999,100000);
-    prepararCountingSort(99999,1000000);
+    prepararCountingSort(99999,10);
+    //prepararCountingSort(99999,10000);
+    //prepararCountingSort(99999,100000);
+    //prepararCountingSort(99999,1000000);
+    
     return 0;
 }
 
@@ -19,8 +20,9 @@ void prepararCountingSort(int k,int tamanhoDoVetor){
     
     clock_t tempo;
     int QtdOperacoesDeComparacao = 0;
-    
-    int vetor[tamanhoDoVetor]; //O tamanho do vetor é definido na chamada da função
+    int *vetor ;
+    vetor= (int*) calloc(tamanhoDoVetor,sizeof(int));
+ 
 
     
     //geração dos vetores de forma aleatório e conforme o tamanho fornecido
@@ -33,15 +35,20 @@ void prepararCountingSort(int k,int tamanhoDoVetor){
     }
 
     tempo = clock();
+    
     countingSort(vetor,k,tamanhoDoVetor,&QtdOperacoesDeComparacao);
+    
     tempo = clock()-tempo;
 
     
 
-    printf ("O tempo de execuçao para %d elementos foi de  ",tamanhoDoVetor);
-    printf(" %f segundos",((float)tempo)/((CLOCKS_PER_SEC)));
+    printf ("O tempo de execucao para %d elementos foi de  ",tamanhoDoVetor);
+    printf(" %f milisegundos",((double)tempo)/((CLOCKS_PER_SEC)/1000));
+    printf(". A quantidade de operacoes de comparacao realizadas ehde %d\n",QtdOperacoesDeComparacao);
     
-    
+    //printarVetor(vetor);
+
+    free(vetor);
 
 }
 
@@ -52,30 +59,32 @@ void countingSort(int *a, int k,int tamanhoDoVetor, int *QtdOperacoesDeComparaca
     //Vetor de armazenamento temporário com tamanho K
     int c[k];
     //Vetor que armazena a ordem de saído
-    int b[tamanhoDoVetor];
+    int *b;
+    b = (int *) calloc(tamanhoDoVetor,sizeof(int));
 
     
-    for(int i=0;i<k;i++){
+    for(int i=0;i<k;i++,*QtdOperacoesDeComparacao+=1){
         c[i] =0;
     }
-   
+    
+    
+    
     //Ainda que no slide se peça para fazer começando com i=1, vou fazer começando com 0 pq os arrays contam a partir de 0
-    for(int i=0;i<tamanhoDoVetor;i++){
-        int pos = a[i];
+    for(int i=0;i<tamanhoDoVetor;i++,*QtdOperacoesDeComparacao+=1){
         c[a[i]]= c[a[i]]+1;
         
     }
     
 
     //cada valor a partir da casa 0 se soma com o valor anterior, e assim se vai acumulando a soma de todos
-    for(int i=1;i<k;i++){
+    for(int i=1;i<k;i++,*QtdOperacoesDeComparacao+=1){
         c[i] = c[i]+c[i-1];
     }
 
     
     
     //Ele vai decrementar até o zero, e não até o 1, porque o array se conta a partir de 0
-    for(int i=tamanhoDoVetor-1;i>=0;i--){
+    for(int i=tamanhoDoVetor-1;i>=0;i--,*QtdOperacoesDeComparacao+=1){
         
        
         
@@ -87,13 +96,15 @@ void countingSort(int *a, int k,int tamanhoDoVetor, int *QtdOperacoesDeComparaca
     
     //acessa o endereço de memória da variável "vetor" e faz com que os valores sejam ordenados da mesma forma como na variável "c" que está dentro dessa atual função
     for(int i=0;i<tamanhoDoVetor;i++){
-        a[i]=b[i];
+        prinf("%d",b[i])
     }
+    
+    
     
 }
 
-void printarVetor(int vetor[]){
-    for(int i=0;i<sizeof(vetor);i++){
+void printarVetor(int *vetor){
+    for(int i=0;i<vetor;i++){
         printf("%d\n",vetor[i]);
     }
 }
